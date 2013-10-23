ac97828ba599c292d60d0ed93712cf94fcfbb739
Kolja Schwenghagen <koljasch@mss.icics.ubc.ca>
2013-10-23 14:36:21 -0700

    Changes in test config

---
+++ acj/static/test/config/karma-e2e-dev.conf.js
@@ -0,0 +1,28 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '../',
+
+    files: [
+            'e2e/*.js'
+    ],
+
+    frameworks: ['ng-scenario', 'jasmine'],
+
+    browsers: ['PhantomJS'],
+
+    singleRun: true,
+
+    proxies: {
+      '/': 'http://127.0.0.1:5001/'
+    },
+
+    urlRoot: '/_karma_/',
+    
+    reporters: ['dots', 'junit'],
+    
+    junitReporter: {
+      outputFile: 'test_out/e2e.xml',
+      suite: 'e2e'
+    }
+  });
+};
