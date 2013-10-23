3f5d9d91fd65caccca4d49b1e8a0c02f9017e4d2
Kolja Schwenghagen <koljasch@mss.icics.ubc.ca>
2013-10-23 13:23:48 -0700

    Port as environment variable

--- runacj.py
+++ runacj.py
@@ -1,5 +1,7 @@
 from acj import app
+import os
 
+port = os.getenv('PORT', 8080)
 #app.run(debug=True)
-app.run('0.0.0.0',8080,debug=True)
+app.run('0.0.0.0',port,debug=True)
 #app.run('0.0.0.0',8080)
