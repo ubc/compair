72cbd9c4bb9f8ec169608acb6c216454cf276480
John Hsu <john.hsu@ubc.ca>
2014-03-10 12:49:05 -0700

    Example Angluar module meant for template use.
    We should take advantage of Angular's module system for code
    organization. The project can be split into page/route modules. This is
    an example module to be used a template for converting the rest of the
    application into a properly modularized Angular app.

---
+++ acj/static/modules/example/example-module.js
@@ -0,0 +1,25 @@
+// TODO
+// Insert short explanation of what this module does, for this module, it'd be:
+// An example/template of how to create functionally isolated modules in
+// Angular for organizing code.
+
+// Isolate this module's creation by putting it in an anonymous function
+(function() {
+
+// TODO 
+// Create the module with a unique name.
+// The module needs a unique name that prevents conflicts with 3rd party modules
+// We're using "ubc.ctlt.acj" as the project's prefix, followed by the module 
+// name.
+var module = angular.module('ubc.ctlt.acj.exampleModule', []);
+
+/***** Providers *****/
+// TODO declare providers here, e.g.:
+// module.factory(...)
+
+/***** Controllers *****/
+// TODO declare controllers here, e.g.:
+// module.controller(...)
+
+// End anonymous function
+})();
