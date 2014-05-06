80d8338556e8fb5cb9eab995993da0e4a5d120e9
Pan Luo <pan.luo@ubc.ca>
2014-05-06 15:22:58 -0700

    Add ability to load settings from environment
    Also fixed the loading Image package from Pillow

--- acj/settings.py
+++ acj/settings.py
@@ -1,13 +1,24 @@
+import os
+
+
+def load_setting(default, env_list):
+    value = default
+    for env in env_list:
+        value = os.environ.get(env, value)
+    return value
+
+
 DATABASE = {'drivername': 'mysql',
-                        'host': 'localhost',
-                        'port': '80',
-                        'username': 'acj',
-                        'password': '',
-                        'database': 'acj'}
+            'host': load_setting('localhost', ['OPENSHIFT_MYSQL_DB_HOST']),
+            'port': load_setting('3306', ['OPENSHIFT_MYSQL_DB_PORT']),
+            'username': load_setting('acj', ['OPENSHIFT_MYSQL_DB_USERNAME']),
+            'password': load_setting('acj', ['OPENSHIFT_MYSQL_DB_PASSWORD']),
+            'database': load_setting('acj', ['OPENSHIFT_GEAR_NAME']),
+            }
 
 DATABASE_TEST = {'drivername': 'mysql',
-                        'host': 'localhost',
-                        'port': '80',
-                        'username': 'acj',
-                        'password': '',
-                        'database': 'acj_test'}
+                 'host': 'localhost',
+                 'port': '3306',
+                 'username': 'acj',
+                 'password': '',
+                 'database': 'acj_test'}
