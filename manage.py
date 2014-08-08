24513826b922dfc803a5f581a8ea53e7e9b0cec0
John Hsu <john.hsu@ubc.ca>
2014-08-08 15:56:22 -0700

    Fix manage.py requiring sample data module.
    Sample data module was moved into database since there's provisions for
    sample data in there already.

--- manage.py
+++ manage.py
@@ -1,14 +1,12 @@
 from flask.ext.script import Manager, Server
 
 from acj.manage.database import manager as database_manager
-from acj.manage.sample_data import manager as sample_data_manager
 from acj import create_app
 
 
 manager = Manager(create_app)
 # register sub-managers
 manager.add_command("database", database_manager)
-manager.add_command("sampledata", sample_data_manager)
 manager.add_command("runserver", Server(port=8080))
 
 if __name__ == "__main__":
