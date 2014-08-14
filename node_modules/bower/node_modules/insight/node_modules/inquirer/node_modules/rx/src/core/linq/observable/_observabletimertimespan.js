06fdc7adb466138b619385d4e13a45bfaa986403
Pan Luo <pan.luo@ubc.ca>
2014-08-14 12:23:46 -0700

    Remove bower_components and node_modules
    As they are both external dependencies and can be installed by running:
    make deps

--- node_modules/bower/node_modules/insight/node_modules/inquirer/node_modules/rx/src/core/linq/observable/_observabletimertimespan.js
+++
@@ -1,9 +0,0 @@
-    function observableTimerTimeSpan(dueTime, scheduler) {
-        var d = normalizeTime(dueTime);
-        return new AnonymousObservable(function (observer) {
-            return scheduler.scheduleWithRelative(d, function () {
-                observer.onNext(0);
-                observer.onCompleted();
-            });
-        });
-    }
