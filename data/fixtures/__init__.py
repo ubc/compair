67f2885ce19d034ba983589553018f415e6b9206
John Hsu <john.hsu@ubc.ca>
2014-09-04 17:31:01 -0700

    Update default sample data generator for 3 rounds.
    Tweaking it to work better with the default 3 rounds requirement by
    putting in more students. Still not really satisifactory, since the data
    is more 'test' than 'sample', should make this a test data fixture
    instead.

--- data/fixtures/__init__.py
+++ data/fixtures/__init__.py
@@ -34,50 +34,50 @@ class DefaultFixture(object):
 		DefaultFixture.DEFAULT_CRITERIA = CriteriaFactory(user=DefaultFixture.ROOT_USER)
 
 class SampleDataFixture(object):
-	COURSES = None
-	INSTRUCTOR = None
-	STUDENTS = None
+	COURSE_NAMES = ["CDEF102", "BCDE101","ABCD100", "DEFG103", "EFGH104"]
+	INSTRUCTOR_NAMES = ["instructor1"]
+	STUDENT_NAMES = ["student1", "student2", "student3", "student4", "student5", "student6",
+					 "student7", "student8"]
 	def __init__(self):
 		# create courses
-		course_names = ["CDEF102", "BCDE101","ABCD100", "DEFG103", "EFGH104"]
-		self.COURSES = []
-		for course_name in course_names:
+		self.courses = []
+		for course_name in self.COURSE_NAMES:
 			course = CoursesFactory(name=course_name, description=course_name + " Course Description")
-			self.COURSES.append(course)
-		# create an instructor
-		self.INSTRUCTOR = UsersFactory(username="instructor1",
-			usertypeforsystem=DefaultFixture.SYS_ROLE_INSTRUCTOR)
+			self.courses.append(course)
+		# create instructors
+		for instructor_name in self.INSTRUCTOR_NAMES:
+			self.instructor = UsersFactory(username=instructor_name,
+				usertypeforsystem=DefaultFixture.SYS_ROLE_INSTRUCTOR)
 		# create students
-		student_names = ["student1", "student2", "student3", "student4", "student5", "student6"]
-		self.STUDENTS = []
-		for student_name in student_names:
+		self.students = []
+		for student_name in self.STUDENT_NAMES:
 			student = UsersFactory(username=student_name,
 				usertypeforsystem=DefaultFixture.SYS_ROLE_NORMAL)
-			self.STUDENTS.append(student)
+			self.students.append(student)
 		# enrol students and instructor in half of the courses, also create questions and answers
 		skip = True
 		generator = TechnobabbleGenerator()
 		content = "This is some place holder content for this question. Yay!"
-		for course in self.COURSES:
+		for course in self.courses:
 			skip = not skip
 			if skip:
 				continue
 			# enrol instructor
-			CoursesAndUsersFactory(user=self.INSTRUCTOR, course=course,
+			CoursesAndUsersFactory(user=self.instructor, course=course,
 				usertypeforcourse=DefaultFixture.COURSE_ROLE_INSTRUCTOR)
 			# enrol students
-			for student in self.STUDENTS:
+			for student in self.students:
 				CoursesAndUsersFactory(user=student, course=course,
 					usertypeforcourse=DefaultFixture.COURSE_ROLE_STUDENT)
 			# create 5 questions by the instructor
 			for i in range(5):
 				minutes=random.randint(0,59)
 				created = datetime.utcnow() - timedelta(days=1,minutes=minutes)
-				post = PostsFactory(course=course, user=self.INSTRUCTOR,
+				post = PostsFactory(course=course, user=self.instructor,
 									content=content, created=created)
 				postforquestion = PostsForQuestionsFactory(post=post, title=generator.get_question())
 				# create answers by each student for this question
-				for student in self.STUDENTS:
+				for student in self.students:
 					minutes=random.randint(0,59)
 					created = datetime.utcnow() - timedelta(minutes=minutes)
 					post = PostsFactory(course=course, user=student, content=generator.get_answer(),
