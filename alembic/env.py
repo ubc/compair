c5b1b10732c00c41791f91f520f4c5091fbbb98c
Pan Luo <pan.luo@ubc.ca>
2014-09-08 12:07:17 -0700

    Make sure db URL is passed as string to alembic

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,8 @@ from acj.configuration import config as acjconfig
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', acjconfig['SQLALCHEMY_DATABASE_URI'])
+
+config.set_main_option('sqlalchemy.url', str(acjconfig['SQLALCHEMY_DATABASE_URI']))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
