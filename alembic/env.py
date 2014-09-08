f9e18673f84b0ed0110c41aad0add41d2be8a87e
Pan Luo <pan.luo@ubc.ca>
2014-09-08 01:56:40 -0700

    Fix the incorrect db url in alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,7 @@ from acj.configuration import config as acjconfig
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('url', acjconfig['SQLALCHEMY_DATABASE_URI'])
+config.set_main_option('sqlalchemy.url', acjconfig['SQLALCHEMY_DATABASE_URI'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
