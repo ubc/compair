d30085b53cdeaf56d2686d9358b4fcf820b814a7
Pan Luo <pan.luo@ubc.ca>
2014-09-08 00:55:03 -0700

    Update database migration script

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,12 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+from acj.configuration import config as acjconfig
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('url', acjconfig['SQLALCHEMY_DATABASE_URI'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -15,7 +18,8 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from acj.core import db
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -35,7 +39,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url)
+    context.configure(url=url, target_metadata=target_metadata)
 
     with context.begin_transaction():
         context.run_migrations()
