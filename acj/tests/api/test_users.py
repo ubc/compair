5b6c8a4c220dc96a06d55a967313aeaba2ff2749
mwytang <michael.tang@ubc.ca>
2015-01-01 12:09:02 -0800

    fixed users test

--- acj/tests/api/test_users.py
+++ acj/tests/api/test_users.py
@@ -284,7 +284,7 @@ class UsersAPITests(ACJTestCase):
 		instructor = UserTypesForSystem.query.filter_by(name=UserTypesForSystem.TYPE_INSTRUCTOR).first()
 		# test updating username, student number, usertype for system - student
 		# will not change the values
-		self.login(self.data.get_authorized_student.username)
+		self.login(self.data.get_authorized_student().username)
 
 		valid = expected.copy()
 		valid['username'] = "wrongUsername"
