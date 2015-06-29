77e3c56e05d45b007b0e6fd91532cb48a03d2832
mwytang <michael.tang@ubc.ca>
2015-06-29 16:30:07 -0700

    Added missing database upgrade script from last commit

---
+++ alembic/versions/4f56a1ca6ff7_create_answer_comment_type_field.py
@@ -0,0 +1,26 @@
+"""create answer comment type field
+
+Revision ID: 4f56a1ca6ff7
+Revises: 4c4e55aabae6
+Create Date: 2015-06-29 15:31:00.833331
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4f56a1ca6ff7'
+down_revision = '4c4e55aabae6'
+
+from alembic import op
+import sqlalchemy as sa
+
+from acj.models import convention
+
+
+def upgrade():
+	op.add_column('AnswersAndComments',
+				sa.Column('type', sa.SmallInteger(), nullable=False, server_default='0', default=0))
+
+
+def downgrade():
+	with op.batch_alter_table('AnswersAndComments', naming_convention=convention) as batch_op:
+		batch_op.drop_column('type')
