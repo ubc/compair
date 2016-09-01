6c3e5ce1a47e0cfb12a875770cb909a424151793
Andrew Gardener <andrew.e.gardener@gmail.com>
2016-09-01 11:04:17 -0700

    Fix class list table sort order
    Empty fields will now be pushed to the bottom of the list when sorting by that field

--- acj/static/modules/common/common-module.js
+++ acj/static/modules/common/common-module.js
@@ -1,5 +1,18 @@
 (function() {
 
-    angular.module('ubc.ctlt.acj.common', []);
+var module =angular.module('ubc.ctlt.acj.common', []);
+
+module.filter("emptyToEnd", function () {
+    return function (array, key) {
+        if(!angular.isArray(array)) return;
+        var present = array.filter(function (item) {
+            return item[key];
+        });
+        var empty = array.filter(function (item) {
+            return !item[key]
+        });
+        return present.concat(empty);
+    };
+});
 
 })();
