3f7fccd14457c05119d248afcbf1f80058c10e96
Andrew Gardener <andrew.e.gardener@gmail.com>
2016-09-02 11:41:20 -0700

    Fix class list sort order
    Will secondary sort by first and last name to help keep the results in a consistent order
    Also reversing occurs after the null/empty values to bottom fix to again keep the sort order consistent

--- acj/static/modules/common/common-module.js
+++ acj/static/modules/common/common-module.js
@@ -3,15 +3,16 @@
 var module =angular.module('ubc.ctlt.acj.common', []);
 
 module.filter("emptyToEnd", function () {
-    return function (array, key) {
+    return function (array, key, reversed) {
         if(!angular.isArray(array)) return;
+        reversed = reversed || false;
         var present = array.filter(function (item) {
             return item[key];
         });
         var empty = array.filter(function (item) {
             return !item[key]
         });
-        return present.concat(empty);
+        return reversed ? empty.concat(present) : present.concat(empty);
     };
 });
 
