98946d230ec7cf346538eb4ccc39053e972fb5bc
Andrew Gardener <andrew.e.gardener@gmail.com>
2016-12-08 13:50:07 -0800

    Fix environment variable issue

--- celery_worker.py
+++ celery_worker.py
@@ -5,7 +5,7 @@ app = create_app(skip_endpoints=True, skip_assets=True)
 
 # needed to get flask debugging working with uwsgi in docker
 # (since it ignores FLASK_DEBUG & DEBUG)
-if os.environ['DEV'] == '1':
+if os.environ.get('DEV') == '1':
     app.debug = True
 
 app.app_context().push()
--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = create_app()
 
 # needed to get flask debugging working with uwsgi in docker
 # (since it ignores FLASK_DEBUG & DEBUG)
-if os.environ['DEV'] == '1':
+if os.environ.get('DEV') == '1':
     app.config['DEBUG'] = True
     app.debug = True
