8cbfe3a69679f2bb19c976d3e520c5fd52f738cb
Andrew Gardener <andrew.e.gardener@gmail.com>
2016-12-16 11:55:41 -0800

    Fix mange grade function
    only recalculate grades  for active courses & assignments

--- compair/manage/grades.py
+++ compair/manage/grades.py
@@ -12,7 +12,7 @@ def generate(course_id=None, all=False):
     courses = []
     if course_id != None:
         course = Course.query.get(course_id)
-        if course:
+        if course and course.active:
             courses = [course]
         else:
             print("No course found with that ID")
@@ -24,10 +24,12 @@ def generate(course_id=None, all=False):
         return
 
     for course in courses:
-        print("Generating grades for course: " + course.name)
-        for assignment in course.assignments:
-            print("--- Generating grades for assignment: " + assignment.name)
-            assignment.calculate_grades()
-        course.calculate_grades()
-        print("")
+        if course.active:
+            print("Generating grades for course: " + course.name)
+            for assignment in course.assignments:
+                if assignment.active:
+                    print("--- Generating grades for assignment: " + assignment.name)
+                    assignment.calculate_grades()
+            course.calculate_grades()
+            print("")
     print("Done.")
