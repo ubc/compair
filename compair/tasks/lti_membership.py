0ed234b10159ba617d5227a8e7031e9e8deb6257
Andrew Gardener <andrew.e.gardener@gmail.com>
2016-12-16 13:44:59 -0800

    Improve error handling for lti delayed tasks
    LTI membership and outcome requests will handle server connectivity errors better. If celery is set to always eager, then process will continue despite timeout and connective errors (no retry is attempted).

--- compair/tasks/lti_membership.py
+++ compair/tasks/lti_membership.py
@@ -1,13 +1,28 @@
+import requests
+
 from compair.core import celery, db
 from compair.models import Course, LTIMembership
 from flask import current_app
 
-@celery.task(ignore_result=True)
-def update_lti_course_membership(course_id):
+@celery.task(bind=True, ignore_result=True)
+def update_lti_course_membership(self, course_id):
     course = Course.query.get(course_id)
     if course:
         current_app.logger.info("Begin LTI Membership update for course with id: "+str(course_id)+" named: "+course.name)
-        LTIMembership.update_membership_for_course(course)
+
+        try:
+            LTIMembership.update_membership_for_course(course)
+
+        except requests.exceptions.ConnectTimeout as error:
+            current_app.logger.info("Failed LTI Membership update for course with id: "+str(course_id)+". "+str(error))
+            if not self.request.is_eager:
+                self.retry(error)
+
+        except requests.exceptions.ConnectionError as error:
+            current_app.logger.info("Failed LTI Membership update for course with id: "+str(course_id)+". "+str(error))
+            if not self.request.is_eager:
+                self.retry(error)
+
         current_app.logger.info("Compelted LTI Membership update for course with id: "+str(course_id)+" named: "+course.name)
     else:
         current_app.logger.info("Failed LTI Membership update for course with id: "+str(course_id)+". record not found.")
--- compair/tasks/lti_outcomes.py
+++ compair/tasks/lti_outcomes.py
@@ -1,31 +1,60 @@
+import requests
+
 from compair.core import celery, db
 from compair.models import LTIConsumer, LTIOutcome, CourseGrade, AssignmentGrade
 from flask import current_app
 
-@celery.task(ignore_result=True)
-def update_lti_course_grades(lti_consumer_id, sourcedid_and_grades):
+@celery.task(bind=True, ignore_result=True)
+def update_lti_course_grades(self, lti_consumer_id, sourcedid_and_grades):
     lti_consumer = LTIConsumer.query.get(lti_consumer_id)
     if lti_consumer:
         current_app.logger.info("Begin LTI Outcomes grade update for lti_consumer: "+str(lti_consumer.id)+" named: "+lti_consumer.tool_consumer_instance_name)
         for (lis_result_sourcedid, course_grade_id) in sourcedid_and_grades:
-            course_grade = CourseGrade.query.find(course_grade_id)
-            grade = course_grade.grade if course_grade else 0.0
+            if course_grade_id:
+                course_grade = CourseGrade.query.get(course_grade_id)
+                grade = course_grade.grade if course_grade else 0.0
 
-            current_app.logger.debug("Posting grade for lis_result_sourcedid: "+lis_result_sourcedid)
-            LTIOutcome.post_replace_result(lti_consumer, lis_result_sourcedid, grade)
+                current_app.logger.debug("Posting grade for lis_result_sourcedid: "+lis_result_sourcedid)
+
+                try:
+                    LTIOutcome.post_replace_result(lti_consumer, lis_result_sourcedid, grade)
+
+                except requests.exceptions.ConnectTimeout as error:
+                    current_app.logger.error("Failed grade update for lis_result_sourcedid: " + lis_result_sourcedid + " with grade: "+str(grade) + ". "+str(error))
+                    if not self.request.is_eager:
+                        self.retry(error)
+
+                except requests.exceptions.ConnectionError as error:
+                    current_app.logger.error("Failed grade update for lis_result_sourcedid: " + lis_result_sourcedid + " with grade: "+str(grade) + ". "+str(error))
+                    if not self.request.is_eager:
+                        self.retry(error)
+
     else:
         current_app.logger.info("Failed LTI Outcomes grade update for lti_consumer with id: "+str(lti_consumer_id)+". record not found.")
 
-@celery.task(ignore_result=True)
-def update_lti_assignment_grades(lti_consumer_id, sourcedid_and_grades):
+@celery.task(bind=True, ignore_result=True)
+def update_lti_assignment_grades(self, lti_consumer_id, sourcedid_and_grades):
     lti_consumer = LTIConsumer.query.get(lti_consumer_id)
     if lti_consumer:
         current_app.logger.info("Begin LTI Outcomes grade update for lti_consumer: "+str(lti_consumer.id)+" named: "+lti_consumer.tool_consumer_instance_name)
         for (lis_result_sourcedid, assignment_grade_id) in sourcedid_and_grades:
-            assignment_grade = AssignmentGrade.query.find(assignment_grade_id)
-            grade = assignment_grade.grade if assignment_grade else 0.0
+            if assignment_grade_id:
+                assignment_grade = AssignmentGrade.query.get(assignment_grade_id)
+                grade = assignment_grade.grade if assignment_grade else 0.0
 
-            current_app.logger.debug("Posting grade for lis_result_sourcedid: "+lis_result_sourcedid)
-            LTIOutcome.post_replace_result(lti_consumer, lis_result_sourcedid, grade)
+                current_app.logger.debug("Posting grade for lis_result_sourcedid: "+lis_result_sourcedid)
+
+                try:
+                    LTIOutcome.post_replace_result(lti_consumer, lis_result_sourcedid, grade)
+
+                except requests.exceptions.ConnectTimeout as error:
+                    current_app.logger.error("Failed grade update for lis_result_sourcedid: " + lis_result_sourcedid + " with grade: "+str(grade) + ". "+str(error))
+                    if not self.request.is_eager:
+                        self.retry(error)
+
+                except requests.exceptions.ConnectionError as error:
+                    current_app.logger.error("Failed grade update for lis_result_sourcedid: " + lis_result_sourcedid + " with grade: "+str(grade) + ". "+str(error))
+                    if not self.request.is_eager:
+                        self.retry(error)
     else:
         current_app.logger.info("Failed LTI Outcomes grade update for lti_consumer with id: "+str(lti_consumer_id)+". record not found.")
