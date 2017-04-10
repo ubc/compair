b146be2dd08fe75ac1f66b600db02e79b57fa363
Pan Luo <pan.luo@ubc.ca>
2017-04-10 14:51:15 -0700

    Update deploy script to fix CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-if [ -n "$TRAVIS_TAG" ]
-then
-    # tag image with git tag with # removed
-    TAG=${TRAVIS_TAG/\#/}
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
     # deploy pull rquest build for review
     TAG="pr-$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT"
-else
+    ENV_NAME="review/$TRAVIS_PULL_REQUEST"
+elif [ "$TRAVIS_EVENT_TYPE" == "push" ]; then
     # deploy master or branch images
     TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+    ENV_NAME=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "staging"; else echo $TRAVIS_BRANCH ; fi`
+else
+    # no deploy for other event types
+    exit 0
 fi
+
+# clean tags
+TAG=${TAG//[^a-zA-Z0-9-\._]/}
+ENV_NAME=${ENV_NAME//[^a-zA-Z0-9-\._ ]/}
 
 curl -X POST \
      -F token=$DEPLOYMENT_TOKEN \
      -F ref=master \
      -F "variables[EVENT_TYPE]=$TRAVIS_EVENT_TYPE" \
-     -F "variables[PULL_REQUEST]=$TRAVIS_PULL_REQUEST" \
+     -F "variables[ENV_NAME]=$ENV_NAME" \
+     -F "variables[BUILD_NUMBER]=$TRAVIS_BUILD_NUMBER" \
      -F "variables[app__image__tag]=$TAG" \
      -F "variables[worker__image__tag]=$TAG" \
      https://repo.code.ubc.ca/api/v3/projects/366/trigger/builds
