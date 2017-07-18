22004c81e6471d0af3c034cff6f53bdf97aa7690
Andrew Gardener <andrew.e.gardener@gmail.com>
2017-07-18 11:50:10 -0700

    Fix assignment comparison count issue

--- compair/models/assignment.py
+++ compair/models/assignment.py
@@ -83,7 +83,7 @@ class Assignment(DefaultTableMixin, UUIDMixin, ActiveMixin, WriteTrackingMixin):
 
     @hybrid_property
     def compared(self):
-        return self.compare_count > 0
+        return self.all_compare_count > 0
 
     def completed_comparison_count_for_user(self, user_id):
         comparison_count = self.comparisons \
@@ -254,9 +254,19 @@ class Assignment(DefaultTableMixin, UUIDMixin, ActiveMixin, WriteTrackingMixin):
             group="counts"
         )
 
-        cls.compare_count = column_property(
+        cls.all_compare_count = column_property(
             select([func.count(Comparison.id)]).
             where(Comparison.assignment_id == cls.id),
+            deferred=True,
+            group="counts"
+        )
+
+        cls.compare_count = column_property(
+            select([func.count(Comparison.id)]).
+            where(and_(
+                Comparison.assignment_id == cls.id,
+                Comparison.completed == True
+            )),
             deferred=True,
             group="counts"
         )
