b27acfa234bf7195c3d1b07d69f101019adbf71b
Andrew Gardener <andrew.e.gardener@gmail.com>
2017-10-31 13:54:06 -0700

    Update report for v0.9.x
    Also back port score management function

--- compair/manage/report.py
+++ compair/manage/report.py
@@ -2,20 +2,23 @@
     Report Generator
 """
 import csv
+import elo
+from compair.algorithms import ScoredObject
+from compair.algorithms.score import calculate_score_1vs1
+import numbers
 
 from flask_script import Manager
-from sqlalchemy import and_
+from sqlalchemy import and_, asc
 from sqlalchemy.orm import aliased
 
 from compair.models import Score, Answer, Criterion, Comparison, \
-    Course, User, UserCourse
+    Course, Assignment, User, UserCourse, ScoringAlgorithm
 
 manager = Manager(usage="Generate Reports")
 
-
+"""
 @manager.option('-c', '--course', dest='course_id', help='Specify a course ID to generate report from.')
 def create(course_id):
-    """Creates report"""
     course_name = ''
     if course_id:
         course_name = Course.query.with_entities(Course.name).filter_by(id=course_id).scalar()
@@ -89,6 +92,136 @@ def create(course_id):
     )
 
     print('Done.')
+"""
+
+@manager.option('-a', '--assignment', dest='assignment_id', help='Specify a Assignment ID to generate report from.')
+def create(assignment_id):
+    """Creates report"""
+    if not assignment_id:
+        raise RuntimeError("Assignment with ID {} is not found.".format(assignment_id))
+
+    assignment = Assignment.query.filter_by(id=assignment_id).first()
+    if not assignment:
+        raise RuntimeError("Assignment with ID {} is not found.".format(assignment_id))
+    criteria = assignment.criteria
+
+    file_name = assignment.course.name.replace('"', '') + '_' + assignment_id + '_'
+
+    scores = Score.query \
+        .with_entities(Answer.user_id, Answer.id,
+                      Criterion.id, Criterion.name,
+                      Score.score, Score.rounds, Score.wins, Score.loses, Score.opponents) \
+        .join(Score.answer) \
+        .join(Score.criterion) \
+        .filter(and_(
+            Answer.assignment_id == assignment.id,
+            Answer.draft == False,
+            Answer.practice == False
+        )) \
+        .order_by(Answer.assignment_id, Criterion.id, Answer.user_id)
+
+    write_csv(
+        file_name + 'scores.csv',
+        ['User Id', 'Answer Id', 'Criterion Id', 'Criterion', 'Score', 'Rounds', 'Wins', 'Loses', 'Opponents'],
+        scores
+    )
+
+    # replay comparisons for real scores at every step
+    comparisons_output = []
+    criterion_scores = {
+        criterion.id: {} for criterion in criteria
+    }
+    past_comparisons = {
+        criterion.id: [] for criterion in criteria
+    }
+
+    comparisons = Comparison.query \
+        .filter_by(
+            assignment_id=assignment.id,
+            completed=True
+        ) \
+        .order_by(Comparison.criterion_id, Comparison.modified, Comparison.user_id) \
+        .all()
+
+    for comparison in comparisons:
+        criterion = next(criterion for criterion in criteria if criterion.id == comparison.criterion_id)
+        answer1_id = comparison.answer1_id
+        answer2_id = comparison.answer2_id
+
+        answer1_score_before = criterion_scores[comparison.criterion_id].get(answer1_id, ScoredObject(
+            key=answer1_id,
+            score=elo.INITIAL,
+            variable1=elo.INITIAL,
+            variable2=None,
+            rounds=0,
+            wins=0,
+            loses=0,
+            opponents=0
+        ))
+        answer2_score_before = criterion_scores[comparison.criterion_id].get(answer2_id, ScoredObject(
+            key=answer2_id,
+            score=elo.INITIAL,
+            variable1=elo.INITIAL,
+            variable2=None,
+            rounds=0,
+            wins=0,
+            loses=0,
+            opponents=0
+        ))
+
+        if comparison.winner_id:
+            other_comparisons = []
+            for pc in past_comparisons[comparison.criterion_id]:
+                if pc.key1 in [answer1_id, answer2_id] or pc.key2 in [answer1_id, answer2_id]:
+                    other_comparisons.append(pc)
+
+            result_1, result_2 = calculate_score_1vs1(
+                package_name=ScoringAlgorithm.elo.value,
+                key1_scored_object=answer1_score_before,
+                key2_scored_object=answer2_score_before,
+                winning_key=comparison.winner_id,
+                other_comparison_pairs=[c for c in other_comparisons]
+            )
+            criterion_scores[criterion.id][result_1.key] = result_1
+            criterion_scores[criterion.id][result_2.key] = result_2
+
+        answer1_score_after = criterion_scores[comparison.criterion_id][answer1_id]
+        answer2_score_after = criterion_scores[comparison.criterion_id][answer2_id]
+
+        past_comparisons[criterion.id].append(comparison.convert_to_comparison_pair())
+
+        comparisons_output.append([
+            comparison.user_id, criterion.id, criterion.name,
+            answer1_id, answer1_score_before.score, answer1_score_after.score,
+            answer2_id, answer2_score_before.score, answer2_score_after.score,
+            comparison.winner_id, comparison.modified
+        ])
+
+    write_csv(
+        file_name + 'comparisons.csv',
+        ['User Id', 'Criterion Id', 'Criterion',
+         'Answer 1', 'Score 1 Before', 'Score 1 After',
+         'Answer 2', 'Score 2 Before', 'Score 2 After',
+         'Winner', 'Timestamp'],
+        comparisons_output
+    )
+
+
+    query = User.query \
+        .join(User.user_courses) \
+        .with_entities(User.id, User.student_number) \
+        .filter(UserCourse.course_id == assignment.course_id) \
+        .order_by(User.id)
+
+    users = query.all()
+
+    write_csv(
+            file_name + 'users.csv',
+            ['User Id', 'Student #'],
+            users
+    )
+
+    print('Done.')
 
 
 def write_csv(filename, headers, data):
@@ -99,4 +232,7 @@ def write_csv(filename, headers, data):
         )
         report_writer.writerow(headers)
         for d in data:
-            report_writer.writerow(d)
+            output = []
+            for o in d:
+                output.append(o)
+            report_writer.writerow(output)
