1521deeaa95df24bc27ee43c55ea222fa4e92a01
Clarence Ho <clarence.ho@ubc.ca>
2017-11-28 13:26:16 -0800

    Merge Alembic upgrade files

---
+++ alembic/versions/f6145781f130_merge_add_uuid_and_min_delta_algo.py
@@ -0,0 +1,22 @@
+"""merge add uuid and min delta algo
+
+Revision ID: f6145781f130
+Revises: ('20381bf0bd54', '10cceff97b06')
+Create Date: 2017-11-28 21:23:20.410400
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'f6145781f130'
+down_revision = ('20381bf0bd54', '10cceff97b06')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
