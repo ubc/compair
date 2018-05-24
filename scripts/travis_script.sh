729bb87088d8b4d6c0dd5257c69068fb0631db0b
Andrew Gardener <andrew.e.gardener@gmail.com>
2018-05-24 15:29:55 -0700

    Fix travis docker image deployment
    Based on 52868b4ac9fbfa4f74994aab75c081871a62fbf7

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -5,12 +5,9 @@ set -e -o pipefail
 if [ "${INTEGRATION_TEST}" = "true" ]; then
     node_modules/.bin/gulp test:ci
 elif [ "${DOCKER}" = "true" ]; then
-    # test image in dev mode, e.g. settings from docker-compose
-    curl -sSfL http://localhost:8080 | grep '<title ng-bind=".*">ComPAIR Learning Application</title>' > /dev/null && curl -sSf http://localhost:8080/api/healthz > /dev/null
     # test image in prod mode
-    docker-compose stop app
-    docker run -d --name compair_app_prod --network compair_default -e DATABASE_URI=mysql+pymysql://compair:compair@db:3306/compair ubcctlt/compair-app
-    curl -sSfL http://localhost:8080 | grep '<title ng-bind=".*">ComPAIR Learning Application</title>' > /dev/null && curl -sSf http://localhost:8080/api/healthz > /dev/null
+    docker-compose -f deploy/docker/docker-compose.yml up -d; sleep 10;
+    curl -sSL http://localhost > /dev/null && curl -sS http://localhost/api/healthz > /dev/null
 else
     nosetests
     node_modules/karma/bin/karma start compair/static/test/config/karma.conf.js --single-run --browsers PhantomJS
