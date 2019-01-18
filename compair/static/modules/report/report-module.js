4164158bb0cf044379930c25c39054fd989f1dd5
Letitia Englund <letitia.englund@gmail.com>
2019-01-18 15:23:28 -0800

    Apply to download reports form

--- compair/static/modules/report/report-module.js
+++ compair/static/modules/report/report-module.js
@@ -41,41 +41,65 @@ module.controller(
         var all = {'id': 'all', 'name': 'All Assignments'};
         var allGroups = {'name': 'All Groups', 'id': 'all'};
         $scope.types = [
-            {'id': 'participation', 'name': 'Basic Participation Report'},
-            {'id': 'participation_stat', 'name': 'Participation Report for Research Teams'},
-            {'id': 'peer_feedback', 'name': 'Compiled Peer Feedback Report'}
+            {'id': 'participation', 'name': 'Basic Participation (best for grading purposes)'},
+            {'id': 'participation_stat', 'name': 'Advanced Participation (best for research purposes)'},
+            {'id': 'peer_feedback', 'name': 'Compiled Peer Feedback'}
         ];
 
         $scope.changeReport = function() {
             $scope.reportFile = null;
         };
 
-        $scope.getAssignments = function() {
+        $scope.getGroupsAssignments = function() {
             $scope.reportFile = null;
-            if ($scope.report.course_id == null) {
+            if ($scope.report.course_id === null || !$scope.report.course_id) {
                 $scope.assignments = [];
+                $scope.groups = [];
                 return;
             }
-            GroupResource.get({'courseId': $scope.report.course_id}).$promise.then(
-                function (ret) {
-                    $scope.report.group_id = 'all';
-                    $scope.groups = ret.objects;
-                    if ($scope.groups && $scope.groups.length > 0) {
-                        $scope.groups.push(allGroups);
-                    }
-                }
-            );
             AssignmentResource.get({'courseId': $scope.report.course_id}).$promise.then(
                 function (ret) {
-                    $scope.report.assignment = null;
+                    $scope.report.assignment = 'all';
                     if (ret.objects.length > 0) {
                         ret.objects.push(all);
                     }
                     $scope.assignments = ret.objects;
                 }
             );
+            GroupResource.get({'courseId': $scope.report.course_id}).$promise.then(
+                function (ret) {
+                    $scope.report.group_id = 'all';
+                    $scope.groups = ret.objects;
+                    if ($scope.groups && $scope.groups.length > 0) {
+                        $scope.groups.push(allGroups);
+                    } else {
+                        $scope.groups = [];
+                    }
+                }
+            );
         };
 
+        // decide on showing inline errors for download report form
+        $scope.showErrors = function($event, formValid, noAssignments) {
+
+            // show error if invalid form
+            if (!formValid || noAssignments) {
+                
+                // don't submit
+                $event.preventDefault();
+                
+                // set helper text and Toast
+                $scope.helperMsg = "Sorry, this report couldn't be run yet, but you're almost there. Simply update any highlighted information above and then try again.";
+                $scope.helperTstrTitle = "Sorry, this report couldn't be run yet";
+                $scope.helperTstrMsg = "...but you're almost there. Simply update the highlighted information and then try again.";
+                
+                // display messages
+                $scope.saveAttempted = true;
+                Toaster.warning($scope.helperTstrTitle, $scope.helperTstrMsg);
+            }
+            
+        };
+        
         $scope.reportSubmit = function() {
             $scope.reportFile = null;
             $scope.submitted = true;
@@ -93,6 +117,7 @@ module.controller(
                 }
             ).finally(function() {
                 $scope.submitted = false;
+                $scope.saveAttempted = false;
             });
         };
     }
