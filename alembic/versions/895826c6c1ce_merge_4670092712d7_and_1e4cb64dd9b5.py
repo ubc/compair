136eeafdd7a055527241bfa3f036b3e0a7058c6c
Clarence Ho <clarence.ho@ubc.ca>
2019-07-23 13:54:18 -0700

    Merge Alembic heads
    - merge alembic heads after pulling in #866

---
+++ alembic/versions/895826c6c1ce_merge_4670092712d7_and_1e4cb64dd9b5.py
@@ -0,0 +1,22 @@
+"""Merge 4670092712d7 and 1e4cb64dd9b5
+
+Revision ID: 895826c6c1ce
+Revises: ('4670092712d7', '1e4cb64dd9b5')
+Create Date: 2019-07-23 20:50:38.916720
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '895826c6c1ce'
+down_revision = ('4670092712d7', '1e4cb64dd9b5')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
