98e8ef5cad49f8b0e589cfa9c26c4acd5131c806
Letitia Englund <letitia.englund@gmail.com>
2019-08-13 14:44:49 -0700

    Changes from usability testing
    (roll back comparison form changes in large part)

--- compair/static/modules/comparison/comparison-module.js
+++ compair/static/modules/comparison/comparison-module.js
@@ -136,62 +136,62 @@ module.controller(
         );
         
         // default to showing answer pair at 50-50 split
-        $scope.expand = "none";
-        $scope.expandAnswer = function(whichOne) {
+        //$scope.expand = "none";
+        //$scope.expandAnswer = function(whichOne) {
             
             //show the left or right answer full-width or go back to 50-50 default
-            switch (whichOne) {
-                case 'left':
-                    $scope.expand = "left";
-                    break;
-                case 'right':
-                    $scope.expand = "right";
-                    break;
-                default:
-                    $scope.expand = "none";
-                    break;
-            }
-            
-        };
+        //    switch (whichOne) {
+        //        case 'left':
+        //            $scope.expand = "left";
+        //            break;
+        //        case 'right':
+        //            $scope.expand = "right";
+        //            break;
+        //        default:
+        //            $scope.expand = "none";
+        //            break;
+        //    }
+        //    
+        //};
         
         /* enable answer pair height buttons to start */
-        $scope.noShrink = false;
-        $scope.noGrow = false;
+        //$scope.noShrink = false;
+        //$scope.noGrow = false;
         
         /* to change answer pair height, find current height and add or substract set amount */
-        $scope.changeHeight = function(direction) {
-            
-            var answersList = document.getElementsByClassName('scrollable-answer');
-            angular.forEach(answersList, function(elem) {
-                
-                var style = window.getComputedStyle(elem, null);
-                var currentHeight = (style.getPropertyValue('height')).slice(0, -2);
-                var growBy = 150;
-                
-                if (direction == "up") {
-                    newHeight = parseInt(currentHeight)+growBy;
-                    $scope.adjustHeight = { "height": newHeight + "px" };
-                }
-                if (direction == "down" && currentHeight > 200) {
-                    newHeight = parseInt(currentHeight)-growBy;
-                    $scope.adjustHeight = { "height": newHeight + "px" };
-                }
-                
-                /* area cannot be less than 200px or more than 950p */
-                if (newHeight <= 200) {
-                    $scope.noShrink = true;
-                    $scope.noGrow = false;
-                } else {
-                    $scope.noShrink = false;
-                    $scope.noGrow = false;
-                }
-                if (newHeight >= 950) {
-                    $scope.noGrow = true;
-                }
-           
-            });
-        
-        };
+        //$scope.changeHeight = function(direction) {
+        //    
+        //    var answersList = document.getElementsByClassName('scrollable-answer');
+        //    angular.forEach(answersList, function(elem) {
+        //        
+        //        var style = window.getComputedStyle(elem, null);
+        //        var currentHeight = (style.getPropertyValue('height')).slice(0, -2);
+        //        var growBy = 150;
+        //        
+        //        if (direction == "up") {
+        //            newHeight = parseInt(currentHeight)+growBy;
+        //            $scope.adjustHeight = { "height": newHeight + "px" };
+        //        }
+        //        if (direction == "down" && currentHeight > 200) {
+        //            newHeight = parseInt(currentHeight)-growBy;
+        //            $scope.adjustHeight = { "height": newHeight + "px" };
+        //        }
+        //        
+        //        /* area cannot be less than 200px or more than 950p */
+        //        if (newHeight <= 200) {
+        //            $scope.noShrink = true;
+        //            $scope.noGrow = false;
+        //        } else {
+        //            $scope.noShrink = false;
+        //            $scope.noGrow = false;
+        //        }
+        //        if (newHeight >= 950) {
+        //            $scope.noGrow = true;
+        //        }
+        //   
+        //    });
+        //
+        //};
 
         $scope.trackExited = function() {
             LearningRecordStatementHelper.exited_comparison_question(
@@ -209,9 +209,9 @@ module.controller(
                 $event.preventDefault();
 
                 // set helper text and Toast
-                $scope.helperMsg = "Sorry, this comparison couldn't be saved yet, but you're almost there. Simply update any highlighted information above and then try again.";
+                $scope.helperMsg = "Sorry, this comparison couldn't be saved yet, but you're almost there. Simply update any highlighted information in Step 1) or Step 2) above and then try again.";
                 $scope.helperTstrTitle = "Sorry, this comparison couldn't be saved yet";
-                $scope.helperTstrMsg = "...but you're almost there. Simply update the highlighted information and then try again.";
+                $scope.helperTstrMsg = "...but you're almost there. Simply update the highlighted information in Step 1) or Step 2) of the form and then try again.";
 
                 // display messages
                 $scope.saveAttempted = true;
