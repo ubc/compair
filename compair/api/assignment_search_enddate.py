b1db0d2e3b8533b29f0e4b0dc943757e07915992
TueHoang <tuehoang@yahoo.com>
2022-06-30 11:40:39 -0700

    Update to use System TZ as default

--- compair/api/assignment_search_enddate.py
+++ compair/api/assignment_search_enddate.py
@@ -6,7 +6,6 @@ import json
 from flask import jsonify
 import pytz
 
-
 from bouncer.constants import READ, EDIT, CREATE, DELETE, MANAGE
 from flask import Blueprint, current_app
 from flask_bouncer import can
@@ -26,6 +25,7 @@ from compair.models import Assignment, Course, Criterion, AssignmentCriterion, A
 from .util import new_restful_api, get_model_changes, pagination_parser
 
 from datetime import datetime
+import time
 
 assignment_search_enddate_api = Blueprint('assignment_search_enddate_api', __name__)
 api = new_restful_api(assignment_search_enddate_api)
@@ -45,13 +45,13 @@ class AssignmentRootAPI1(Resource):
         search_date_assignment_parser = RequestParser()
         search_date_assignment_parser.add_argument('compare_start', default=datetime.now().strftime("%Y-%m-%d"))
         search_date_assignment_parser.add_argument('compare_end', default=datetime.now().strftime("%Y-%m-%d"))
-        search_date_assignment_parser.add_argument('compare_localTimeZone', default='America/Vancouver')
+        search_date_assignment_parser.add_argument('compare_localTimeZone', default=time.strftime('%Z'))
 
         args = search_date_assignment_parser.parse_args()
 
         end_date = datetime.now().strftime("%Y-%m-%d 00:00:00")
         start_date = datetime.now().strftime("%Y-%m-%d")
-        compare_localTimeZone = 'UTC'
+        compare_localTimeZone = time.strftime('%Z')
 
         if (args['compare_localTimeZone']):
             compare_localTimeZone = str(args['compare_localTimeZone'])
@@ -59,12 +59,12 @@ class AssignmentRootAPI1(Resource):
         if validate(args['compare_end']):
             end_date = str(args['compare_end']) + ' 00:00:00'
 
-            ##convert this to UTC
+            ##convert this to System TZ
             local = pytz.timezone(compare_localTimeZone)
             naive = datetime.strptime(end_date, "%Y-%m-%d %H:%M:%S")
             local_dt = local.localize(naive, is_dst=None)
-            utc_dt = local_dt.astimezone(pytz.utc)
-            end_date = str(utc_dt)
+            systemTZ_dt = local_dt.astimezone(pytz.timezone(time.strftime('%Z')))
+            end_date = str(systemTZ_dt)
 
         if validate(args['compare_start']):
             start_date = str(args['compare_start'])
@@ -73,7 +73,7 @@ class AssignmentRootAPI1(Resource):
         engine = create_engine(db_url, pool_size=5, pool_recycle=3600)
         conn = engine.connect()
 
-        sql_text = str("SELECT JSON_OBJECT('course_name', t1.name,'name', t2.name,'answer_start', date_format(CONVERT_TZ(t2.answer_start, 'UTC','" + compare_localTimeZone + "'), '%%b %%d, %%Y'),'answer_end', date_format(CONVERT_TZ(t2.answer_end, 'UTC','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'),'compare_start', date_format(CONVERT_TZ(t2.compare_start, 'UTC','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'), 'compare_end', date_format(CONVERT_TZ(t2.compare_end, 'UTC','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'), 'self_eval_end', date_format(CONVERT_TZ(t2.self_eval_end, 'UTC','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'), 'self_eval_start', date_format(CONVERT_TZ(t2.self_eval_start, 'UTC','" + compare_localTimeZone + "'), '%%b %%d, %%Y')) FROM course as t1, assignment as t2 WHERE (t1.id = t2.course_id) AND (t2.active=TRUE AND t1.active=TRUE) AND (t2.compare_end >=  '" + end_date + "' OR answer_end >= '" + end_date + "' OR self_eval_end >= '" + end_date +  "');");
+        sql_text = str("SELECT JSON_OBJECT('course_name', t1.name,'name', t2.name,'answer_start', date_format(CONVERT_TZ(t2.answer_start, '" + time.strftime('%Z') + "','" + compare_localTimeZone + "'), '%%b %%d, %%Y'),'answer_end', date_format(CONVERT_TZ(t2.answer_end, '" + time.strftime('%Z') + "','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'),'compare_start', date_format(CONVERT_TZ(t2.compare_start, '" + time.strftime('%Z') + "','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'), 'compare_end', date_format(CONVERT_TZ(t2.compare_end, '" + time.strftime('%Z') + "','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'), 'self_eval_end', date_format(CONVERT_TZ(t2.self_eval_end, '" + time.strftime('%Z') + "','" + compare_localTimeZone + "'),  '%%b %%d, %%Y'), 'self_eval_start', date_format(CONVERT_TZ(t2.self_eval_start, '" + time.strftime('%Z') + "','" + compare_localTimeZone + "'), '%%b %%d, %%Y')) FROM course as t1, assignment as t2 WHERE (t1.id = t2.course_id) AND (t2.active=TRUE AND t1.active=TRUE) AND (t2.compare_end >=  '" + end_date + "' OR answer_end >= '" + end_date + "' OR self_eval_end >= '" + end_date +  "');");
 
         result = conn.execute(sql_text)
 
